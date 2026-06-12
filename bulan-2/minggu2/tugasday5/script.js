f9e6fddc7be4eefd3404f81847f5bd2199b76d5e
hammam09 <mmamm0403@gmail.com>
2026-06-12 15:01:29 +0700

    feat: add missing comments for exercises in script.js

--- bulan-2/minggu2/tugasday5/script.js
+++ bulan-2/minggu2/tugasday5/script.js
@@ -1,13 +1,16 @@
+// Soal Nomor 1
 console.log("Soal nomor 1");
 for (number = 1; number <= 10; number++){
     console.log("Hitungan ke : " + number);
 }
 
+// Soal Nomor 2
 console.log("Soal nomor 2");
 for (number = 10; number >= 1; number--){
     console.log("Hitungan ke : " + number);
 }
 
+// Soal Noomor 3
 console.log("Soal nomor 3");
 for (number = 1; number <= 20; number++){
     if(number % 2 !== 0){
@@ -16,6 +19,7 @@ for (number = 1; number <= 20; number++){
     console.log(number);
 }
 
+// Soal Nomor 4
 console.log("Soal nomor 4");
 for (number = 1; number <= 20; number++){
     if(number % 2 === 0){
@@ -24,11 +28,13 @@ for (number = 1; number <= 20; number++){
     console.log(number);
 }
 
+// Soal Nomor 5
 console.log("Soal nomor 5");
 for (let number = 1; number <= 10; number++){
     console.log(`5 * ${number} = ${5 * number}`);
 }
 
+// Soal Nomor 6
 console.log("Soal nomor 6");
 for (let sum = 1; sum <= 50; sum++){
     console.log(sum + sum);
