4880929ee7166d937d8aa49b04c24f9803072e05
hammam09 <mmamm0403@gmail.com>
2026-06-17 09:47:59 +0700

    feat: menambahkan keterangan nomor di setiap jawaban

--- bulan-2/minggu2/evalming2/script.js
+++ bulan-2/minggu2/evalming2/script.js
@@ -3,7 +3,7 @@
 
 
 // 1. Tipe Data 
-
+console.log("Nomor 1");
 let nilaiAngka = 75;
 let nilaiString = "75";
 let nilaiBoolean = true;
@@ -14,6 +14,7 @@ console.log(typeof nilaiBoolean);
 
 // 2. Penjelasan dari kode berikut
 
+console.log("Nomor 2");
 let q = 10;
 let h = "5";
 console.log(q+h);
@@ -22,6 +23,7 @@ console.log(q+h);
 
 // 3. Operator penjumlahan
 
+console.log("Nomor 3");
 let x = 10;
 let y = 4;
 let hasil = ((x + y) * 2) / 4 % 3;
@@ -30,6 +32,7 @@ console.log("Apakah hasil sama dengan 1?", hasil == 1);
 
 // 4. Tentukan hasil kode di bawah
 
+console.log("Nomor 4");
 let g = 8;
 g += 4;
 g *= 2;
@@ -37,6 +40,8 @@ g -= 6;
 console.log(g);
 
 // 5. Genap dan Ganjil
+
+console.log("Nomor 5");
 let Angka = 3;
 if (Angka % 2 === 0) {
     if (Angka > 0) {
@@ -54,6 +59,7 @@ if (Angka % 2 === 0) {
 
 // 6. Sistem Penilaian
 
+console.log("Nomor 6");
 let nilai = 82;
 if (nilai >= 90) {
     console.log("A");
@@ -69,6 +75,7 @@ if (nilai >= 90) {
 
 // 7. Menentukan 3 angka terbesar
 
+console.log("Nomor 7");
 let A = 25;
 let B = 40;
 let C = 90;
@@ -83,6 +90,7 @@ if (A >= B && A >= C) {
 
 // 8. Tentukan output dari kode berikut
 
+console.log("Nomor 8");
 let X = 5;
 let Y = 10;
 let Z = (X > 3 && Y < 15) || X === 8;
@@ -90,8 +98,8 @@ console.log(Z);
 
 // 9. Menentukan tahun
 
+console.log("Nomor 9");
 let tahun = 2026;
-
 if ((tahun % 400 === 0) || (tahun % 4 === 0 && tahun % 100 !== 0)) {
     console.log(tahun + " adalah tahun kabisat");
 } else {
@@ -100,6 +108,7 @@ if ((tahun % 400 === 0) || (tahun % 4 === 0 && tahun % 100 !== 0)) {
 
 // 10. Program FizzBuzz
 
+console.log("Nomor 10");
 for (let i = 1; i <= 50; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
@@ -114,12 +123,14 @@ for (let i = 1; i <= 50; i++) {
 
 // 11. Menghitung nilai z dan d
 
+console.log("Nomor 11");
 let z = 4;
 let d = z++ + ++z;
 console.log(d);
 
 // 12. Menghitung total bilangan ganjil dari 1-100
 
+console.log("Nomor 12");
 let total = 0;
 for (let i = 1; i <= 100; i++) {
     if (i % 2 !== 0) {
@@ -135,6 +146,7 @@ console.log("Total bilangan ganjil:", total);
 // ****
 // *****
 
+console.log("Nomor 13");
 for (let i = 1; i <= 5; i++) {
     let baris = "";
     for (let j = 1; j <= i; j++) {
@@ -145,6 +157,7 @@ for (let i = 1; i <= 5; i++) {
 
 // 14. Menghitung seluruh jumlah elemen yag lebih dari 10
 
+console.log("Nomor 14");
 let angka = [12, 5, 18, 7, 21, 30, 2];
 let jumlah = 0;
 for (let i = 0; i < angka.length; i++) {
@@ -156,6 +169,7 @@ console.log(jumlah);
 
 // 15. Mencari bilangan prima dari 1-100
 
+console.log("Nomor 15");
 for (let angka = 2; angka <= 100; angka++) {
     let prima = true;
     for (let i = 2; i < angka; i++) {
@@ -171,6 +185,7 @@ for (let angka = 2; angka <= 100; angka++) {
 
 // 16. Menentukan output dari kode di bawah
 
+console.log("Nomor 16");
 let a = 2;
 let b = 3;
 let c = 4;
@@ -186,6 +201,7 @@ let c = 4;
 
 // 17. Membuat login sederhana
 
+console.log("Nomor 17");
 let userName = prompt('Masukkan username Anda:');
 let Password = prompt('Masukkan password Anda:');
 if (userName == 'admin' && Password == '123') {
@@ -196,6 +212,7 @@ if (userName == 'admin' && Password == '123') {
 
 // 18. Menentukan Hari dalam 1 bulan selama 1 Tahun
 
+console.log("Nomor 18");
 let bulan = 2;
 switch (bulan) {
   case 1:
@@ -225,6 +242,7 @@ switch (bulan) {
 
 // 19. Menampilan deret angka menggunakan looping
 
+console.log("Nomor 19");
 let deret = 1;
 for (let i = 1; i <= 7; i++) {
   console.log(deret);
@@ -233,8 +251,8 @@ for (let i = 1; i <= 7; i++) {
 
 // 20. Menentukan nilai akhir beserta penjelasan
 
+console.log("Nomor 20");
 let totalNilai = 0;
-
 for (let i = 1; i <= 10; i++) {
   if (i % 2 === 0) {
     totalNilai += i;
@@ -242,5 +260,4 @@ for (let i = 1; i <= 10; i++) {
     totalNilai -= i;
   }
 }
-
 console.log(totalNilai);
